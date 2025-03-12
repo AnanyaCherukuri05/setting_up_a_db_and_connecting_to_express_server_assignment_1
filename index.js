f3fc95f5570a3d681270179f599284f60999d6f0
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-03-12 09:55:50 +0530

    .

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { resolve } = require('path');
-
 const app = express();
-const port = 3010;
-
+require('dotenv').config();
 app.use(express.static('static'));
+const port=process.env.PORT || 3010;
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+mongoose.connect(process.env.mongo_url, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  try {
+    console.log(`Example app listening at http://localhost:${port}`);
+  } catch (error) {
+    console.log(error);
+  }
 });
